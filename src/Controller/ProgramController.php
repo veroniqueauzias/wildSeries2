2260daa98134197ada8b48c32cb47770dcf1d45e
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-09-09 11:29:22 +0200

    method show on programs and start twig view

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
@@ -17,4 +21,16 @@ class ProgramController extends AbstractController
             'website' => 'Wild Séries',
          ]);
     }
+
+
+    /**
+     * @Route("/{id}", methods={"GET"}, requirements={"page"="\d+"}, name="show")
+     */
+    public function show(int $id) : Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+         ]);
+    }
+
 }
