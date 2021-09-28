71dcd16266c65101d9ff613a56cbedd8ee26ed27
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-09-28 16:49:48 +0200

    make categ form work

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
 use App\Entity\Program;
 use App\Form\CategoryType;
-
+use Symfony\Component\HttpFoundation\Request;
 /**
 * @Route("/categories", name="category_")
 */
@@ -35,12 +35,26 @@ class CategoryController extends AbstractController
      *
      * @Route("/ajouter", name="new")
      */
-    public function new() : Response
+    public function new(Request $request): Response
     {
         // Create a new Category Object
         $category = new Category();
         // Create the associated Form
         $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
         // Render the form
         return $this->render('category/new.html.twig', [
             "form" => $form->createView(),
