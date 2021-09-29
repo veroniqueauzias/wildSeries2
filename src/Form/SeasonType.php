518705eafc0435618a43d0a98e1f038f4bac1b53
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-09-29 18:37:01 +0200

    design seasons views and forms

--- src/Form/SeasonType.php
+++ src/Form/SeasonType.php
@@ -6,16 +6,26 @@ use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SeasonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('number')
-            ->add('year')
-            ->add('description')
-            ->add('program')
+            ->add('number', integerType::class, [
+                'label' => 'numéro de la saison',
+            ])
+            ->add('year', integerType::class, [
+                'label' => '1ère année de diffusion'
+            ])
+            ->add('description', textareaType::class, [
+                'label' => 'Résumé'
+            ])
+            ->add('program', null, [
+                'choice_label' => 'title', 
+                'label'=> 'Série'])
         ;
     }
 
