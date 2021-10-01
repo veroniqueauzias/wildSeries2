193b361cdd1b54d4236f573fc633f9f3e1d3fa56
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-10-01 19:00:40 +0200

    episode form season choice display with program name and season number

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -3,20 +3,36 @@
 namespace App\Form;
 
 use App\Entity\Episode;
+use App\Entity\Program;
+use App\Entity\Season;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class EpisodeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('number')
-            ->add('synopsis')
-            ->add('season')
-        ;
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+            ])       
+            ->add('number', IntegerType::class, [
+                'label' => 'numéro de l\'épisode',
+            ])
+            ->add('synopsis', TextareaType::class, [
+                'label' => 'Résumé',
+                ]) 
+            ->add('season', null, [
+                'choice_label' => function(?Season $season) {
+                    return $season->getProgram()->getTitle() . ' ' . 'saison' . ' ' . $season->getNumber();
+                },
+                'label' =>'Saison',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
