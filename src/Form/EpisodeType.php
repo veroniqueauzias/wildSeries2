ff4cba2ea4569fe1d40b4b76ffa4c3c51b3e07a7
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-10-02 18:56:47 +0200

    new episode form design

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -11,7 +11,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class EpisodeType extends AbstractType
 {
