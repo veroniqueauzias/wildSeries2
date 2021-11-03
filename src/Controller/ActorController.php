abc0b4b81cd92f5a141de64efea396c00ef3e93b
Veronique AUZIAS <veroniqueauzias@gmail.com>
2021-11-03 17:26:50 +0100

    generate actor index

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Actor;
+use App\Form\ActorType;
+use App\Repository\ActorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/actor")
+ */
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("/", name="actor_index", methods={"GET"})
+     */
+    public function index(ActorRepository $actorRepository): Response
+    {
+        return $this->render('actor/index.html.twig', [
+            'actors' => $actorRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="actor_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $actor = new Actor();
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($actor);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('actor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('actor/new.html.twig', [
+            'actor' => $actor,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="actor_show", methods={"GET"})
+     */
+    public function show(Actor $actor): Response
+    {
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="actor_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Actor $actor): Response
+    {
+        $form = $this->createForm(ActorType::class, $actor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('actor_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('actor/edit.html.twig', [
+            'actor' => $actor,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="actor_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Actor $actor): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$actor->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($actor);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('actor_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
